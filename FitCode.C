378943d9dd360435854ec8fb27189790355a35f5
Sangeet-Pannu <101436536+Sangeet-Pannu@users.noreply.github.com>
2022-12-21 19:16:04 -0500

    Update FitCode.C
    Just Cleaned up the Programs

--- FitCode.C
+++ FitCode.C
@@ -21,21 +21,9 @@
 #include "TTree.h"
 
 /*
-SOFTWARE: 112 COULEX DATA FITTING FUNCTION.
-Author: Sangeet-Pal Pannu
-
-FUNCTIONALITY: This code implements two skewed gaussian fitting,
-in which each peak is fit using two skewed gaussians in order to
-yield a high degree of variablity when fitting the skewed tail feature.
-
-- Fits two peaks together on top a quadratic background.
+FitCode.C Will be used to evaluate the Attenuation Factors for 
+Each of the Detector Pair Configurations.
 */
-
-//Linear background
-//par[0] = Slope
-//par[1] = Constant
-
- // additional peaks to be added to system besides groundstate.
 
 float Wtheo[29];
 float y[29];
@@ -63,98 +51,9 @@ float R4MMJ2J1 = 0;
 float R4LLJ2J3 = 0;
 float R4LMJ2J3 = 0;
 float R4MMJ2J3 = 0;
-                      
 
-
-/*
-    data.root is the output file for which the histograms will be written to
-    for the next setup of the analysis of angular correlations.
-    
-    exp.txt is the experimental coinicidence fits on the respective cascade
-    one is looking at, with the implemented EVENT MIXING.
-    
-    comb??? is the simulation files for the respective cascade.
-*/
-//angs "String" changes between "Fangs" (FIPPS ANGLE ID) and "Iangs" (IFIN ANGLE ID).
-
-TGraph* CreateHisto(
-   std::string data = "./data.root",
-   std::string exp = "./Normexp.txt",std::string angs = "/home/Sangeet/Master_project/100Ru-Data/Angular_Correlations/Fangs.txt"){
-
-   TGraph2D graphexp(exp.c_str());
-   TGraph2D graphangs(angs.c_str());
-
-   auto binEdgesexp = graphangs.GetX();
-   
-   auto valuesexp = graphexp.GetY();
-   
-   auto errorexp = graphexp.GetZ();
-
-   auto histexp = new TGraphErrors(graphexp.GetN()-1,binEdgesexp,valuesexp,nullptr,errorexp);
-
-
-   for(int j =0; j<graphexp.GetN();j++){
-   	y[j] = valuesexp[j];
-   	yr[j] = errorexp[j];
-   }
-
- 
-   return histexp;
-
-}
-
-
-   
-
-
-
-// implementing a simple normalization.
-double AngCorFunction_M1(double *x,double* par)
-{
-	double rad = x[0];   
-	double Pl2 = (1.0/2.0)*(3.*pow(rad,2)-1.);
-	double Pl4 = (1.0/8.0)*(35.*pow(rad,4)-30.*pow(rad,2)+3.);
-
-	return (par[4])*(1 + par[2]*par[0]*Pl2+ par[3]*par[1]*Pl4);
-
-}
-
-//implementing Sambu's method
-double AngCorFunction_M2(double *x,double* par)
-{   
-    R2LLJ2J1 = -0.1707;
-                R2LMJ2J1 = -0.5051;
-                R2MMJ2J1 = 0.4482;
-                R2LLJ2J3 = -0.5976;
-                R2LMJ2J3 = 0.0;
-                R2MMJ2J3 = 0.0;
-                R4LLJ2J1 = -0.0085;
-                R4LMJ2J1 = 0.0627;
-                R4MMJ2J1 = -0.0297;
-                R4LLJ2J3 = -1.0690;
-                R4LMJ2J3 = 0.0;
-                R4MMJ2J3 = 0.0;
-    
-
-    double rad = x[0];   
-    double Pl2 = (1.0/2.0)*(3*pow(rad,2)-1);
-    double Pl4 = (1.0/8)*(35*pow(rad,4)-30*pow(rad,2)+3);
-
-    //?-2-0 Any with mixing ratios
-  
-    float a2 = ((R2LLJ2J1 + 2 * delta1 * R2LMJ2J1 + pow(delta1, 2) * R2MMJ2J1) * (R2LLJ2J3 + 2 * delta2 * R2LMJ2J3 + pow(delta2, 2) * R2MMJ2J3) / (1 + pow(delta1, 2)) / (1 + pow(delta2, 2)));
-    float a4 = ((R4LLJ2J1 + 2 * delta1 * R4LMJ2J1 + pow(delta1, 2) * R4MMJ2J1) * (R4LLJ2J3 + 2 * delta2 * R4LMJ2J3 + pow(delta2, 2) * R4MMJ2J3) / (1 + pow(delta1, 2)) / (1 + pow(delta2, 2)));
-
-    double theo_W = (1 + par[0]*a2*Pl2+ par[1]*a4*Pl4);
-    
-    return theo_W;
-
-}
-
-//All cascades other than 0-2-0
-double AngCorFunction_M3(double *x,double* par)
-{
-    if (J1 == 1 && J2 == 2 && J3 == 0) {
+//=================================================================+|
+if (J1 == 1 && J2 == 2 && J3 == 0) {
         R2LLJ2J1 = 0.4183;
         R2LMJ2J1 = 0.9354;
         R2MMJ2J1 = -0.2988;
@@ -220,7 +119,65 @@ double AngCorFunction_M3(double *x,double* par)
                 R4LMJ2J3 = 0.0;
                 R4MMJ2J3 = 0.0;
             
-            }
+            }                    
+//=================================================================+|
+
+/*
+    exp.txt is the experimental coinicidence fits on the respective cascade
+    one is looking at, with the implemented EVENT MIXING.
+*/
+//angs "String" changes between "Fangs" (FIPPS ANGLE ID) and "Iangs" (IFIN ANGLE ID).
+
+TGraph* CreateHisto(std::string exp = "./Normexp.txt",std::string angs = "/home/Sangeet/Master_project/100Ru-Data/Angular_Correlations/Fangs.txt"){
+   TGraph2D graphexp(exp.c_str());
+   TGraph2D graphangs(angs.c_str());
+
+   auto binEdgesexp = graphangs.GetX();
+   auto valuesexp = graphexp.GetY();
+   auto errorexp = graphexp.GetZ();
+   auto histexp = new TGraphErrors(graphexp.GetN()-1,binEdgesexp,valuesexp,nullptr,errorexp);
+
+   for(int j =0; j<graphexp.GetN();j++){
+      y[j] = valuesexp[j];
+      yr[j] = errorexp[j];
+   }
+
+ 
+   return histexp;
+
+}
+
+// implementing a simple normalization.
+double AngCorFunction_M1(double *x,double* par)
+{
+   double rad = x[0];   
+   double Pl2 = (1.0/2.0)*(3.*pow(rad,2)-1.);
+   double Pl4 = (1.0/8.0)*(35.*pow(rad,4)-30.*pow(rad,2)+3.);
+
+   return (par[4])*(1 + par[2]*par[0]*Pl2+ par[3]*par[1]*Pl4);
+
+}
+
+//implementing Sambu's method
+double AngCorFunction_M2(double *x,double* par)
+{   
+    double rad = x[0];   
+    double Pl2 = (1.0/2.0)*(3*pow(rad,2)-1);
+    double Pl4 = (1.0/8)*(35*pow(rad,4)-30*pow(rad,2)+3);
+
+    float a2 = ((R2LLJ2J1 + 2 * delta1 * R2LMJ2J1 + pow(delta1, 2) * R2MMJ2J1) * (R2LLJ2J3 + 2 * delta2 * R2LMJ2J3 + pow(delta2, 2) * R2MMJ2J3) / (1 + pow(delta1, 2)) / (1 + pow(delta2, 2)));
+    float a4 = ((R4LLJ2J1 + 2 * delta1 * R4LMJ2J1 + pow(delta1, 2) * R4MMJ2J1) * (R4LLJ2J3 + 2 * delta2 * R4LMJ2J3 + pow(delta2, 2) * R4MMJ2J3) / (1 + pow(delta1, 2)) / (1 + pow(delta2, 2)));
+
+    double theo_W = (1 + par[0]*a2*Pl2+ par[1]*a4*Pl4);
+    
+    return theo_W;
+
+}
+
+//All cascades other than 0-2-0
+double AngCorFunction_M3(double *x,double* par)
+{
+    
 
     double rad = x[0];   
     double Pl2 = (1.0/2.0)*(3*pow(rad,2)-1);
@@ -241,8 +198,7 @@ double AngCorFunction_M3(double *x,double* par)
 
 void FitCode(){
     //Uses the exp file to create a graph which will be used to fit the angular correlation results on.
-    auto c1 = new TCanvas("c1", "Angular Distribution of FIPPS SAMBU");
-    //auto c2 = new TCanvas("c2", "Angular Distribution",1000,500);
+    auto c1 = new TCanvas("c1", "Angular Distribution With Using SAMBU");
     c1->Divide(2,1);
     c1->cd(1);
 
@@ -275,7 +231,7 @@ void FitCode(){
 
     cout << "\nQ2 = " << fitfunc->GetParameter(0) <<" +/- "<< fitfunc->GetParError(0) << endl;
     cout << "Q4 = " << fitfunc->GetParameter(1) <<" +/- "<< fitfunc->GetParError(1) << endl;
-    fhist->SetTitle("Angular Distribution of FIPPS SAMBU");
+    fhist->SetTitle("Angular Distribution With Using SAMBU");
     fhist->Draw("AP");
     
 
@@ -290,12 +246,14 @@ void FitCode(){
     fitfunc1->SetParName(3,"Q4");
     fitfunc1->SetParName(4,"Norm C");   
 
-    fitfunc1->FixParameter(0,0.1020408163265306); // Background slope
-    fitfunc1->FixParameter(1,0.009070294784580489); // Background offset 
-    fitfunc1->SetParameter(2,0.9); // Background slope
+    fitfunc1->FixParameter(0,0.1020408163265306); //A2 Value Gotten From GRIFFIN TOOLS Angular Distribution Calculator
+    fitfunc1->FixParameter(1,0.009070294784580489); //A4 Value Gotten From GRIFFIN TOOLS Angular Distribution Calculator
+    
+    fitfunc1->SetParameter(2,0.9); 
     fitfunc1->SetParameter(3,0.6);
     fitfunc1->SetParLimits(3,0.1,1);
     fitfunc1->SetParLimits(2,0.1,1);
+    
     fitfunc1->SetParameter(4,1);
 
     fhist1->Fit("fitfunc1","REM");
@@ -314,4 +272,3 @@ void FitCode(){
     
 
 }
-
